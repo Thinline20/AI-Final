286aa28aaa12662cd7304a9cb233ff9ca4ce4dc2
Thinline20 <thinline20@gmail.com>
2022-11-02 22:02:01 +0900

    Change javascript package manager from yarn to pnpm

--- projects/backend/main.py
+++ projects/backend/main.py
@@ -1,5 +1,7 @@
+import uvicorn
 from typing import Union
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
@@ -12,3 +14,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn_config = uvicorn.Config("main:app", port=8000, log_level="info")
+    server = uvicorn.Server(uvicorn_config)
+
+    server.run()
